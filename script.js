d582d96efde07ffc3c0a076d9d3537f0019ac993
Shayan Biswas <106047915+shbis@users.noreply.github.com>
2022-12-24 16:54:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,13 @@ var result = document.getElementById("result");
 
 for (let i = 0; i < li.length; i++) {
     li[i].addEventListener("click", function () {
-
         if (this.innerHTML == "=") {
-
             result.innerHTML = eval(result.innerHTML);
-
         } else {
             if (this.innerHTML == "÷") {
                 result.innerHTML += "/";
             } else if (this.innerHTML == "x") {
                 result.innerHTML += "*";
-
             } else if (this.innerHTML == 'C') {
                 result.innerHTML = "";
             }
